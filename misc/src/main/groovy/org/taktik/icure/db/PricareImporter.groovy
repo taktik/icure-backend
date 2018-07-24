6eba0b720e691700b9b3d604864fb985661b0d9c
dorian-figac <dorian@figac.be>
2018-07-24 12:37:56 +0200

    import pricare parties and patients first draft

--- misc/src/main/groovy/org/taktik/icure/db/PricareImporter.groovy
+++ misc/src/main/groovy/org/taktik/icure/db/PricareImporter.groovy
@@ -1,23 +1,42 @@
 package org.taktik.icure.db
 
 import groovy.sql.Sql
+import org.json.JSON
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.taktik.icure.entities.HealthcareParty
 import org.taktik.icure.entities.User
 import org.taktik.icure.security.database.ShaAndVerificationCodePasswordEncoder
 
+import org.taktik.icure.entities.*
+
 class PricareImporter extends Importer {
     static void main(String... args) {
-        new PricareImporter().scan(args)
+        def imp = new PricareImporter()
+        imp.keyRoot = "c:\\topaz\\keys"
+        imp.scan(args)
     }
 
     void scan(String... args) {
-        def db = [url:'jdbc:sqlserver://192.168.63.97\\pricaresql;databaseName=modelbird_670_20170713_medinote', user:'MedinoteUser', password:'xyz123', driver:'com.microsoft.sqlserver.jdbc.SQLServerDriver']
+        def db = [url:'jdbc:sqlserver://localhost\\pricaresql;databaseName=modelbird_670_20170713_medinote', user:'MedinoteUser', password:'xyz123', driver:'com.microsoft.sqlserver.jdbc.SQLServerDriver']
         def sql = Sql.newInstance(db.url, db.user, db.password, db.driver)
         def passwordEncoder = new ShaAndVerificationCodePasswordEncoder(256)
+
+        Map<String, User>  users = [:]
         Map<String,HealthcareParty> hcParties = [:]
-        def users = [:]
+        Map<String, Patient> patients = [:]
+        Map<String, List<Invoice>> invoices = [:]
+        Map<String, List<Contact>> contacts = [:]
+        Map<String, List<HealthElement>> healthElements = [:]
+        Map<String, List<Form>> forms = [:]
+        List<Message> messages = []
+        Map<String, Collection<String>> messageDocs = [:]
+        List<Map> docs = []
+        List<AccessLog> accessLogs = []
+
         try {
+
+            // users, parties
+
             sql.eachRow("select * from tblParty") {
                 def id = idg.newGUID().toString()
                 hcParties[id] = new HealthcareParty(
@@ -37,9 +56,71 @@ class PricareImporter extends Importer {
                     )
                 }
             }
+
+            // patients
+
+            sql.eachRow("select top 3 * from tblPat") {
+                def id = idg.newGUID().toString()
+                patients[id] = new Patient(
+                        id: id,
+                        lastName: it.Lname,
+                        firstName: it.Fname,
+                )
+            }
+
+            // invoices
+
+
+            // contacts
+            /*
+
+            sql.eachRow("select * from tblcon") {
+                def id = idg.newGUID().toString()
+                contacts[id] = new Contact(
+                        id: id,
+                        created: it.valueDate.getTime(),
+
+                        encounterType: "CD-CONSULT"
+
+                )
+            }
+
+            ///// motifs
+
+
+            sql.eachRow("select * from tblmotive") {
+                def id = idg.newGUID().toString()
+                def service = [
+                        id: id
+                ]
+                contacts[id].services.add(service)
+            }
+
+            // healthElements
+
+            sql.eachRow("select * from tblhe") {
+                def id = idg.newGUID().toString()
+                healthElements[id] = new HealthElement(
+                        id: id,
+                        descr: it.name,
+                        relevant: it.significance = 2,
+                )
+            }
+            */
+
+            // forms
+
+            // messages
+
+            // messageDocs
+
+            // docs
+
+            // accessLogs
+
         } finally {
             sql.close()
         }
-        doImport(users.values(), hcParties.values())
+        doImport(users.values(), hcParties.values(), patients.values(), invoices, contacts, healthElements, forms, messages, messageDocs, docs, accessLogs)
     }
 }
