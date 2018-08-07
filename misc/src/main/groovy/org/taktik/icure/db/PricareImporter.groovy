9058aa06fef56ea9a5fe5c2148910411f5e1ef4b
dorian-figac <dorian@figac.be>
2018-08-07 15:47:46 +0200

    migrate more fields for user, hcparty, he. Add services to subcontacts list

--- misc/src/main/groovy/org/taktik/icure/db/PricareImporter.groovy
+++ misc/src/main/groovy/org/taktik/icure/db/PricareImporter.groovy
@@ -3,7 +3,13 @@ package org.taktik.icure.db
 import groovy.json.JsonSlurper
 import groovy.sql.Sql
 import org.taktik.icure.entities.base.Code
+import org.taktik.icure.entities.embed.Address
+import org.taktik.icure.entities.embed.AddressType
 import org.taktik.icure.entities.embed.Service
+import org.taktik.icure.entities.embed.ServiceLink
+import org.taktik.icure.entities.embed.SubContact
+import org.taktik.icure.entities.embed.Telecom
+import org.taktik.icure.entities.embed.TelecomType
 import org.taktik.icure.security.database.ShaAndVerificationCodePasswordEncoder
 
 
@@ -50,6 +56,7 @@ class PricareImporter extends Importer {
         def contacts_by_medinoteId = [:]
         def users_by_medinoteId = [:]
         def hcparties_by_medinoteId = [:]
+        String debug_he_topazId_by_medinoteId = [:] // debug: service need to be linked to he to be shown currently
 
         try {
 
@@ -61,7 +68,9 @@ class PricareImporter extends Importer {
                         id: id,
                         lastName: it.Lname,
                         firstName: it.Fname,
-                        civility: it.Title
+                        civility: it.Title,
+                        nihii: it.inami,
+                        ssin: it.natnum,
                 )
                 hcparties_by_medinoteId[it.id] = hcParties[id]
 
@@ -75,6 +84,7 @@ class PricareImporter extends Importer {
                             "type": "database",
                             status: "ACTIVE",  // DEBUG
                             //status: it.inactive ? "DISABLED" : "ACTIVE",
+                            email: it.privEmail,
                             passwordHash: passwordEncoder.encodePassword(it.Password, null)
                     )
 
@@ -89,8 +99,77 @@ class PricareImporter extends Importer {
                 patientMap[it.id] = id
                 patients[id] = new Patient(
                         id: id,
-                        lastName: it.Lname,
+                        created: medinote_date_to_topaz_fuzzydate(it.entryDate),
+                        lastName: it.Lname + " MDN " + (new Date()).format("yyyy-MM-dd HH:mm:ss"), // DEBUG: added date to see last created patient
                         firstName: it.Fname,
+                        author: users_by_medinoteId[it.authorId],
+                        responsible: hcparties_by_medinoteId[it.respid],
+                        active: !it.inactive,
+                        ssin: it.natnum,
+                        gender: it.gender == 1 ? "female" : "male", // TODO: verify matching
+                        partnerName: it.spouseName,
+                        dateOfBirth: it.BirthDate.toInteger(),
+                        dateOfDeath: it.deathDate.toInteger(),
+                        placeOfBirth: it.birthPlace,
+                        //nationality: it.nationality // TODO: convert number to iso codes
+                        externalId: it.patientMap, // FIXME: not sure what externalId is
+                        addresses: [
+                                new Address(
+                                        addressType: AddressType.home,
+                                        street: it.privStreet,
+                                        houseNumber: "", // TODO: split street number or take from admin
+                                        postalCode: it.privPostCode,
+                                        city: it.privCity,
+                                        country: "be",
+                                        telecoms: [
+                                                new Telecom(
+                                                        telecomType: TelecomType.phone,
+                                                        telecomNumber: it.privPhone
+                                                ),
+                                                new Telecom(
+                                                        telecomType: TelecomType.mobile,
+                                                        telecomNumber: it.privGsm
+                                                ),
+                                                new Telecom(
+                                                        telecomType: TelecomType.email,
+                                                        telecomNumber: it.privEmail
+                                                ),
+                                                new Telecom(
+                                                        telecomType: TelecomType.fax,
+                                                        telecomNumber: it.privFax
+                                                ),
+                                        ]
+                                ),
+                                new Address(
+                                        addressType: AddressType.work,
+                                        street: it.busStreet,
+                                        houseNumber: "", // TODO: split street number or take from admin
+                                        postalCode: it.busPostCode,
+                                        city: it.busCity,
+                                        country: "be",
+                                        telecoms: [
+                                                new Telecom(
+                                                        telecomType: TelecomType.phone,
+                                                        telecomNumber: it.busPhone
+                                                ),
+                                                new Telecom(
+                                                        telecomType: TelecomType.mobile,
+                                                        telecomNumber: it.busGsm
+                                                ),
+                                                new Telecom(
+                                                        telecomType: TelecomType.email,
+                                                        telecomNumber: it.busEmail
+                                                ),
+                                                new Telecom(
+                                                        telecomType: TelecomType.fax,
+                                                        telecomNumber: it.busFax
+                                                ),
+                                        ]
+                                )
+
+                        ]
+
+
                 )
             }
 
@@ -106,16 +185,41 @@ class PricareImporter extends Importer {
                     contacts[pid] = []
                 }
                 def con = new Contact(
-                        id: id,
-                        created: it.valueDate.getTime(),
-                        openingDate: medinote_date_to_topaz_fuzzydate(it.valueDate),
-                        closingDate: medinote_date_to_topaz_fuzzydate(it.valueDate),
-                        encounterType: new Code("BE", "TOPAZ-ENCOUNTER", enctype, "1"),
-                        descr: "TEST"
+                    id: id,
+                    created: medinote_date_to_topaz_fuzzydate(it.entryDate),
+                    author: users_by_medinoteId[it.authorId],
+                    responsible: hcparties_by_medinoteId[it.respid],
+                    openingDate: medinote_date_to_topaz_fuzzydate(it.valueDate),
+                    closingDate: medinote_date_to_topaz_fuzzydate(it.valueDate),
+                    encounterType: new Code("TOPAZ-ENCOUNTER", enctype, "1"),
+                    descr: "TEST",
+                    subContacts: [ ]
                 )
                 contacts_by_medinoteId[it.id] = con
                 contacts[pid].add(con)
                 println("Adding contact (medi=${it.id}, tz=${id})")
+
+                // subcontacts
+
+
+
+                /*
+                // forms
+
+                def formid = idg.newGUID().toString()
+
+                //def form = new Form()
+                def form = new Form(
+                        id: formid,
+                        created: medinote_date_to_topaz_fuzzydate(it.entryDate),
+                        author: users_by_medinoteId[it.authorId],
+                        responsible: hcparties_by_medinoteId[it.respid],
+                        descr: "Consultation",
+
+
+                )
+                */
+
             }
 
             // healthElements
@@ -157,8 +261,54 @@ class PricareImporter extends Importer {
                     if (healthElements[pid] == null) {
                         healthElements[pid] = []
                     }
+
+                    def certaintyMap = [
+                        0: "undefined",
+                        1: "certain",
+                        2: "probable",
+                        3: "improbable",
+                        4: "excluded",
+                    ]
+                    def severityMap = [ // FIXME: many values in Kmehr, not sure how to map
+                        0: "undefined",
+                        1: "normal", // degree 1
+                        2: "low", // degree 2
+                        3: "high", // degree 3
+                    ]
+                    def temporalityMap = [ // many values in Kmehr, not sure what to choose
+                       0: "undefined",
+                       1: "acute", // nonrecurring
+                       2: "chronic", // recurring
+                    ]
+                    def itemtype
+                    // FIXME: add more CD-ITEM codes or replace the existing one ? (currently only one code)
+                    if( it.risk) {
+                        itemtype = "risk"
+                    }
+                    if( it.socialrisk) {
+                        itemtype = "socialrisk"
+                    }
+                    if( it.socialrisk) {
+                        itemtype = "socialrisk"
+                    }
+                    switch(it.allergyintoltype) {
+                        case 0: // regular
+                            break
+                        case 1: // allergy
+                            itemtype = "allergy"
+                            break
+                        case 2: // intol
+                            //itemtype = "" // FIXME: no equivalent in kmehr
+                            break
+                        case 3: // professionalexposure
+                            itemtype = "professionalrisk"
+                            break
+                    }
+
                     healthElements[pid].add( new HealthElement(
                             id: topaz_heid,
+                            author: users_by_medinoteId[it.authorId],
+                            responsible: hcparties_by_medinoteId[it.respid],
                             healthElementId: id,
                             descr: "TEST " + it.name.toString(),
                             relevant: it.significance == 2,
@@ -168,7 +318,10 @@ class PricareImporter extends Importer {
                             idClosingContact: topaz_contact_id,
                             idOpeningContact: topaz_contact_id,
                             tags: [
-                                    new Code("CD-ITEM", "healthcareelement", "1")
+                                    new Code("CD-ITEM", itemtype, "1"),
+                                    new Code("CD-TEMPORALITY", temporalityMap[it.temporality], "1"),
+                                    new Code("CD-SEVERITY", severityMap[it.severity], "1"),
+                                    new Code("CD-CERTAINTY", certaintyMap[it.certainty], "1"),
                             ],
                             codes: [
                                     medinote_medicalCodeId_to_topaz_code(it.MedicalCodeId)
@@ -177,9 +330,6 @@ class PricareImporter extends Importer {
                 }
             }
 
-            // forms
-
-            //def form = new Form()
 
             ///////// services
 
@@ -210,7 +360,8 @@ class PricareImporter extends Importer {
                                     ]
                             ],
                     )
-                    contact.services.add(service)
+                    add_service_to_contact(service, contact)
+                    //contact.services.add(service)
                     println("Adding motive (tz-conid=${contact.id}, medi=${it.id}, tz=${id})")
 
                 }
@@ -242,7 +393,8 @@ class PricareImporter extends Importer {
                                     ]
                             ],
                     )
-                    contact.services.add(service)
+                    add_service_to_contact(service, contact)
+                    //contact.services.add(service)
                     println("Adding anamneses (tz-conid=${contact.id}, medi=${it.id}, tz=${id})")
 
                 }
@@ -271,7 +423,8 @@ class PricareImporter extends Importer {
                                     ]
                             ],
                     )
-                    contact.services.add(service)
+                    add_service_to_contact(service, contact)
+                    //contact.services.add(service)
                     println("Adding measure ${measName} (tz-conid=${contact.id}, medi=${it.id}, tz=${id})")
 
                 }
@@ -360,5 +513,25 @@ class PricareImporter extends Importer {
     }
     static long medinote_date_to_topaz_fuzzydate(date) {
         return date.format("yyyyMMdd").toLong() * 1000000
+    }
+
+    void add_service_to_contact(service, contact) {
+        if(contact.subContacts.size() == 0) {
+            contact.subContacts.add(
+                new SubContact(
+                    services: [ ]
+                )
+            )
+
+        } else {
+            assert contact.subContacts[0].services instanceof List
+        }
+        contact.services.add(
+                service
+        )
+        def serv = new ServiceLink(
+                serviceId: service.id
+        )
+        contact.subContacts[0].services.add( serv )
     }
 }
