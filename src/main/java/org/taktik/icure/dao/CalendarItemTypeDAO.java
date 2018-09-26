55034a7c56ad0b93fa4dad35dbb0b770664ef117
dlm-nsi <40821489+BEArbiter@users.noreply.github.com>
2018-09-26 11:40:34 +0200

    fetch Include delete

--- src/main/java/org/taktik/icure/dao/CalendarItemTypeDAO.java
+++ src/main/java/org/taktik/icure/dao/CalendarItemTypeDAO.java
@@ -2,5 +2,8 @@ package org.taktik.icure.dao;
 
 import org.taktik.icure.entities.CalendarItemType;
 
+import java.util.List;
+
 public interface CalendarItemTypeDAO extends GenericDAO<CalendarItemType> {
+    List<CalendarItemType> getAllEntitiesIncludeDelete();
 }
