fe49a4cef803eff4681152b6d572ddcd23656701
Antoine Duchateau <ad@taktik.com>
2018-10-09 08:42:12 +0200

    Fixed secretEmpty saved in db

--- src/main/java/org/taktik/icure/entities/User.java
+++ src/main/java/org/taktik/icure/entities/User.java
@@ -292,6 +292,7 @@ public class User extends StoredDocument implements Principal, Cloneable, Serial
 		return id != null ? id.hashCode() : 0;
 	}
 
+	@JsonIgnore
 	public boolean isSecretEmpty() {
 		return secret == null;
 	}
