d1bae8e35b46bad6ef04f5562e55e9b8e4589163
Nicolas Surleraux <nicolas.surleraux@b12-consulting.com>
2018-10-19 09:20:56 +0200

    DB view correction

--- src/main/resources/org/taktik/icure/dao/impl/js/invoice/By_hcparty_efact_to_be_corrected_map.js
+++ src/main/resources/org/taktik/icure/dao/impl/js/invoice/By_hcparty_efact_to_be_corrected_map.js
@@ -1,14 +1,14 @@
 function isInvoiceBad(doc) {
-  if(!doc.sentDate || doc.invoicingCodes.length === 0) return false;
+  if(doc.invoicingCodes.length === 0) return false;
   return doc.invoicingCodes.every(function(code){
-    return code.canceled
+    return code.resent
   });
 }
 
 map = function (doc) {
 	if (doc.java_type == 'org.taktik.icure.entities.Invoice' && doc.sentMediumType === "efact" && doc.invoiceType ==="mutualfund" && isInvoiceBad(doc) && doc.delegations && Object.keys(doc.delegations).length) {
 		Object.keys(doc.delegations).forEach(function (k) {
-            emit([k, doc.invoiceDate], doc._id)
+      emit([k, doc.invoiceDate], doc._id)
 		});
 	}
 };
