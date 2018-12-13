f9c069acda3f8f18829b4d745a8aedfb9e51875f
LAPSAM5\Sam Jocqué <sam@figac.be>
2018-12-13 16:23:37 +0100

    WIP: medicationscheme : get transaction(set) as JSON TODO: store the JSON as message.document.attachment (see archiving logic listing FF invoicing)

--- src/main/kotlin/org/taktik/icure/services/external/rest/v1/facade/be/KmehrFacade.kt
+++ src/main/kotlin/org/taktik/icure/services/external/rest/v1/facade/be/KmehrFacade.kt
@@ -179,5 +179,4 @@ class KmehrFacade(val mapper: MapperFacade, val sessionLogic: SessionLogic, val 
 				patientId?.let { patientLogic.getPatient(patientId) },
 				mappings ?: HashMap()).map {mapper.map(it, ImportResultDto::class.java)})
 	}
-
 }
