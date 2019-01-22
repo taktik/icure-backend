cfe2aee7efdccdc1c38c8ed883ab2dac611c7d27
Phil Mertens <mertensph@gmail.com>
2019-01-22 16:46:49 +0100

    Ad missing getter/setter

--- src/main/java/org/taktik/icure/services/external/rest/v1/dto/ApplicationSettingsDto.java
+++ src/main/java/org/taktik/icure/services/external/rest/v1/dto/ApplicationSettingsDto.java
@@ -4,5 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ApplicationSettingsDto extends StoredDto {
+
     protected Map<String, String> options = new HashMap<>();
+
+    public Map<String, String> getOptions() {
+        return options;
+    }
+
+    public void setOptions(Map<String, String> options) {
+        this.options = options;
+    }
 }
