c8398c8dd13e97dab2191d70e671a3d70bb9ffed
Cédric Bamps <cedric.bamps@b12-consulting.com>
2019-01-23 14:15:45 +0100

    add missing @Service annotation to ApplicationSettingsLogicImpl

--- src/main/java/org/taktik/icure/logic/impl/ApplicationSettingsLogicImpl.java
+++ src/main/java/org/taktik/icure/logic/impl/ApplicationSettingsLogicImpl.java
@@ -1,10 +1,12 @@
 package org.taktik.icure.logic.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.taktik.icure.dao.ApplicationSettingsDAO;
 import org.taktik.icure.entities.ApplicationSettings;
 import org.taktik.icure.logic.ApplicationSettingsLogic;
 
+@Service
 public class ApplicationSettingsLogicImpl extends GenericLogicImpl<ApplicationSettings, ApplicationSettingsDAO> implements ApplicationSettingsLogic {
 
     private ApplicationSettingsDAO applicationSettingsDAO;
