d388614566affb92d816198a06ac5ede604f8deb
Fabien Zimmer <fabien_zimmer@hotmail.com>
2019-01-25 12:11:41 +0100

    Poudriere database, do NOT take into account

--- src/main/kotlin/org/taktik/icure/config/CouchDbLocalConfig.kt
+++ src/main/kotlin/org/taktik/icure/config/CouchDbLocalConfig.kt
@@ -39,9 +39,9 @@ class CouchDbLocalConfig(val couchDbProperties: CouchDbProperties) {
             .build())
 
     @Bean fun couchdbConfig(couchdbInstance:CouchDbInstance) = StdCouchDbICureConnector("icure-config", couchdbInstance)
-    @Bean fun couchdbBase(couchdbInstance:CouchDbInstance) = StdCouchDbICureConnector("icure-base", couchdbInstance)
-    @Bean fun couchdbPatient(couchdbInstance:CouchDbInstance) = StdCouchDbICureConnector("icure-patient", couchdbInstance)
-    @Bean fun couchdbHealthdata(couchdbInstance:CouchDbInstance) = StdCouchDbICureConnector("icure-healthdata", couchdbInstance)
+    @Bean fun couchdbBase(couchdbInstance:CouchDbInstance) = StdCouchDbICureConnector("icure-base-poudriere", couchdbInstance)
+    @Bean fun couchdbPatient(couchdbInstance:CouchDbInstance) = StdCouchDbICureConnector("icure-patient-poudriere", couchdbInstance)
+    @Bean fun couchdbHealthdata(couchdbInstance:CouchDbInstance) = StdCouchDbICureConnector("icure-healthdata-poudriere", couchdbInstance)
 
     @Bean fun entitiesCacheManager() = NoOpCacheManager()
 }
