73d45dd7dc8ebaacd3d4151412f785b1b49496a6
Fabien Zimmer <fabien_zimmer@hotmail.com>
2019-02-26 08:45:20 +0100

    Before merge master, AVOID this one

--- src/main/kotlin/org/taktik/icure/config/CouchDbLocalConfig.kt
+++ src/main/kotlin/org/taktik/icure/config/CouchDbLocalConfig.kt
@@ -39,9 +39,9 @@ class CouchDbLocalConfig(val couchDbProperties: CouchDbProperties) {
             .build())
 
     @Bean fun couchdbConfig(couchdbInstance:CouchDbInstance) = StdCouchDbICureConnector("icure-config", couchdbInstance)
-    @Bean fun couchdbBase(couchdbInstance:CouchDbInstance) = StdCouchDbICureConnector("icure-base", couchdbInstance)
-    @Bean fun couchdbPatient(couchdbInstance:CouchDbInstance) = StdCouchDbICureConnector("icure-patient", couchdbInstance)
-    @Bean fun couchdbHealthdata(couchdbInstance:CouchDbInstance) = StdCouchDbICureConnector("icure-healthdata", couchdbInstance)
+    @Bean fun couchdbBase(couchdbInstance:CouchDbInstance) = StdCouchDbICureConnector("icure-base-dev-master", couchdbInstance)
+    @Bean fun couchdbPatient(couchdbInstance:CouchDbInstance) = StdCouchDbICureConnector("icure-patient-dev-master", couchdbInstance)
+    @Bean fun couchdbHealthdata(couchdbInstance:CouchDbInstance) = StdCouchDbICureConnector("icure-healthdata-dev-master", couchdbInstance)
 
     @Bean fun entitiesCacheManager() = NoOpCacheManager()
 }
