6a6b233253f87690ed89498d4110fc589042ac8e
Antoine Duchateau <ad@taktik.be>
2019-03-29 15:22:13 +0100

    Fixed tarification and Boolean properties

--- src/main/java/org/taktik/icure/services/external/rest/v1/dto/TypedValueDto.java
+++ src/main/java/org/taktik/icure/services/external/rest/v1/dto/TypedValueDto.java
@@ -29,7 +29,7 @@ public class TypedValueDto implements Serializable, Comparable<TypedValueDto> {
 
 	private TypedValuesType type;
 
-	private Integer booleanValue;
+	private Boolean booleanValue;
 	private Integer integerValue;
 	private Double doubleValue;
 	private String stringValue;
@@ -52,7 +52,7 @@ public class TypedValueDto implements Serializable, Comparable<TypedValueDto> {
 
 		switch (type) {
 			case BOOLEAN :
-				return (T) (booleanValue == null ? null : booleanValue == 1);
+				return (T) booleanValue;
 			case INTEGER :
 				return (T) integerValue;
 			case DOUBLE :
@@ -102,7 +102,7 @@ public class TypedValueDto implements Serializable, Comparable<TypedValueDto> {
 			switch (type) {
 				case BOOLEAN :
 					if (value instanceof Boolean) {
-						booleanValue = ((Boolean) value) ? 1 : 0;
+						booleanValue = (Boolean) value;
 					}
 					break;
 				case INTEGER :
@@ -147,11 +147,11 @@ public class TypedValueDto implements Serializable, Comparable<TypedValueDto> {
 		this.type = type;
 	}
 
-	public Integer getBooleanValue() {
+	public Boolean getBooleanValue() {
 		return booleanValue;
 	}
 
-	public void setBooleanValue(Integer booleanValue) {
+	public void setBooleanValue(Boolean booleanValue) {
 		this.booleanValue = booleanValue;
 	}
 
