8613cf18a198b40f729602f0ff57bed5d21df3ba
Julia Dumur <dumur.julia@gmail.com>
2019-07-02 11:50:50 +0200

    To merge correctly and add of the beginning of addServiceCodesAndTags method

--- src/test/kotlin/org/taktik/icure/be/ehealth/logic/kmehr/sumehr/impl/v20161201/SumehrExportTest.kt
+++ src/test/kotlin/org/taktik/icure/be/ehealth/logic/kmehr/sumehr/impl/v20161201/SumehrExportTest.kt
@@ -4,9 +4,11 @@ import org.junit.Assert.*
 import org.junit.Test
 import org.mockito.Mockito
 import org.taktik.icure.entities.Patient
+import org.taktik.icure.be.ehealth.dto.kmehr.v20161201.be.fgov.ehealth.standards.kmehr.schema.v1.ItemType
+import org.taktik.icure.entities.base.CodeStub
+import org.taktik.icure.entities.embed.Service
 
 class SumehrExportTest {
-
     //The method tested needs a SumehrExport Class to run
     private val sumehrExport = SumehrExport()
 
@@ -25,5 +27,33 @@ class SumehrExportTest {
         //Tests
         assertNotNull(md5)
         assertFalse(md5.isBlank())
+    }
+
+    @Test
+    fun addServiceCodesAndTags() {
+        // Arrange
+        /// First parameter
+        val svc = Service()
+        val code = CodeStub("CD-TRANSACTION", "allergy", "1")
+        svc.codes.add(code)
+
+        /// Second parameter
+        val item = ItemType()
+
+        /// Third parameter
+        val skipCdItem = true;
+
+        /// Fourth parameter
+        val restrictedTypes = listOf("allergy")
+
+        /// Fifth parameter
+        val uniqueTypes = listOf("string")
+
+        /// Sixth parameter
+        val excludedTypes = null
+
+        // Execution
+        sumehrExport.addServiceCodesAndTags(svc, item, skipCdItem, restrictedTypes, uniqueTypes, excludedTypes)
+        //super.addServiceCodesAndTags(svc, item, skipCdItem, restrictedTypes, uniqueTypes, (excludedTypes ?: emptyList()) + listOf("LOCAL", "RELEVANCE", "SUMEHR", "SOAP", "CD-TRANSACTION", "CD-TRANSACTION-TYPE"))
     }
 }
