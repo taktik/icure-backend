9646cad7ca2b382f0cf515a02b6ff75ba915c0e5
Simon Hardy <sha@taktik.com>
2019-10-23 17:44:53 +0200

    new controller

---
+++ src/main/kotlin/org/taktik/icure/services/external/rest/v1/controllers/ClassificationController.kt
@@ -0,0 +1,131 @@
+/*
+ * Copyright (C) 2018 Taktik SA
+ *
+ * This file is part of iCureBackend.
+ *
+ * iCureBackend is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
+ *
+ * iCureBackend is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with iCureBackend.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.taktik.icure.services.external.rest.v1.controllers
+
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
+import ma.glasnost.orika.MapperFacade
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
+import org.taktik.icure.entities.Classification
+import org.taktik.icure.entities.embed.Delegation
+import org.taktik.icure.logic.ClassificationLogic
+import org.taktik.icure.services.external.rest.v1.dto.ClassificationDto
+import org.taktik.icure.services.external.rest.v1.dto.IcureStubDto
+import org.taktik.icure.services.external.rest.v1.dto.embed.DelegationDto
+import java.util.*
+import javax.ws.rs.POST
+import javax.ws.rs.Path
+
+@RestController
+@RequestMapping("/classification")
+@Api(tags = ["classification"])
+class ClassificationController(private val mapper: MapperFacade,
+                               private val classificationLogic: ClassificationLogic) {
+
+    @ApiOperation(nickname = "createClassification", value = "Create a classification with the current user", notes = "Returns an instance of created classification Template.")
+    @PostMapping
+    fun createClassification(@RequestBody c: ClassificationDto): ClassificationDto {
+        val element = classificationLogic.createClassification(mapper.map(c, Classification::class.java))
+                ?: throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Classification creation failed.")
+
+        return mapper.map(element, ClassificationDto::class.java)
+    }
+
+    @ApiOperation(nickname = "getClassification", value = "Get a classification Template")
+    @GetMapping("/{classificationId}")
+    fun getClassification(@PathVariable classificationId: String): ClassificationDto {
+        val element = classificationLogic.getClassification(classificationId)
+                ?: throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Getting classification failed. Possible reasons: no such classification exists, or server error. Please try again or read the server log.")
+
+        return mapper.map(element, ClassificationDto::class.java)
+    }
+
+    @ApiOperation(nickname = "getClassificationByHcPartyId", value = "Get a list of classifications", notes = "Ids are seperated by a coma")
+    @GetMapping("/byIds/{ids}")
+    fun getClassificationByHcPartyId(@PathVariable ids: String): List<ClassificationDto> {
+        val elements = classificationLogic.getClassificationByIds(ids.split(','))
+                ?: throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Getting classification failed. Possible reasons: no such classification exists, or server error. Please try again or read the server log.")
+
+        return elements.map { mapper.map(it, ClassificationDto::class.java) }
+    }
+
+    @ApiOperation(nickname = "findByHCPartyPatientSecretFKeys", value = "List classification Templates found By Healthcare Party and secret foreign keyelementIds.", notes = "Keys hast to delimited by coma")
+    @GetMapping("/byHcPartySecretForeignKeys")
+    fun findByHCPartyPatientSecretFKeys(@RequestParam hcPartyId: String, @RequestParam secretFKeys: String): List<ClassificationDto> {
+        val secretPatientKeys = secretFKeys.split(',').map { it.trim() }
+        val elementList = classificationLogic.findByHCPartySecretPatientKeys(hcPartyId, secretPatientKeys)
+                ?: throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Getting the classification failed. Please try again or read the server log.")
+
+        return elementList.map { mapper.map(it, ClassificationDto::class.java) }
+    }
+
+    @ApiOperation(nickname = "deleteClassifications", value = "Delete classification Templates.", notes = "Response is a set containing the ID's of deleted classification Templates.")
+    @DeleteMapping("/{classificationIds}")
+    fun deleteClassifications(@PathVariable classificationIds: String): List<String> {
+        val ids = classificationIds.split(',')
+        if (ids.isEmpty()) {
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "A required query parameter was not specified for this request.")
+        }
+
+        return classificationLogic.deleteClassifications(HashSet(ids))?.toList()
+                ?: throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Classification deletion failed")
+    }
+
+    @ApiOperation(nickname = "modifyClassification", value = "Modify a classification Template", notes = "Returns the modified classification Template.")
+    @PutMapping
+    fun modifyClassification(@RequestBody classificationDto: ClassificationDto): ClassificationDto {
+        classificationLogic.modifyClassification(mapper.map(classificationDto, Classification::class.java))
+        val modifiedClassification = classificationLogic.getClassification(classificationDto.id)
+                ?: throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Classification modification failed.")
+
+        return mapper.map(modifiedClassification, ClassificationDto::class.java)
+    }
+
+
+    @ApiOperation(nickname = "newDelegations", value = "Delegates a classification to a healthcare party", notes = "It delegates a classification to a healthcare party (By current healthcare party). Returns the element with new delegations.")
+    @POST
+    @Path("/{classificationId}/delegate")
+    fun newDelegations(@PathVariable classificationId: String, @RequestBody ds: List<DelegationDto>): ClassificationDto {
+        classificationLogic.addDelegations(classificationId, ds.map { mapper.map(it, Delegation::class.java) })
+        val classificationWithDelegation = classificationLogic.getClassification(classificationId)
+
+        val succeed = classificationWithDelegation != null && classificationWithDelegation.delegations != null && classificationWithDelegation.delegations.isNotEmpty()
+        if (succeed) {
+            return mapper.map(classificationWithDelegation, ClassificationDto::class.java)
+        } else {
+            throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Delegation creation for classification failed.")
+        }
+    }
+
+    @ApiOperation(nickname = "setClassificationsDelegations", value = "Update delegations in classification", notes = "Keys must be delimited by coma")
+    @PostMapping("/delegations")
+    fun setClassificationsDelegations(stubs: List<IcureStubDto>) {
+        val classifications = classificationLogic.getClassificationByIds(stubs.map { it.id })
+        classifications.forEach { classification ->
+            stubs.find { it.id == classification.id }?.let { stub ->
+                stub.delegations.forEach { (s, delegationDtos) -> classification.delegations[s] = delegationDtos.map { ddto -> mapper.map(ddto, Delegation::class.java) }.toSet() }
+                stub.encryptionKeys.forEach { (s, delegationDtos) -> classification.encryptionKeys[s] = delegationDtos.map { ddto -> mapper.map(ddto, Delegation::class.java) }.toSet() }
+                stub.cryptedForeignKeys.forEach { (s, delegationDtos) -> classification.cryptedForeignKeys[s] = delegationDtos.map { ddto -> mapper.map(ddto, Delegation::class.java) }.toSet() }
+            }
+        }
+        classificationLogic.updateEntities(classifications)
+    }
+}
