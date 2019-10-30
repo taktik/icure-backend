6baf2d31512f942055353ac5e891bc1ac200b5c1
Simon Hardy <sha@taktik.com>
2019-10-30 16:50:49 +0100

    add tag descriptions

--- src/main/kotlin/org/taktik/icure/config/SwaggerConfig.kt
+++ src/main/kotlin/org/taktik/icure/config/SwaggerConfig.kt
@@ -5,17 +5,13 @@ import org.springframework.context.annotation.Configuration
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.service.AuthorizationScope
+import springfox.documentation.service.BasicAuth
+import springfox.documentation.service.SecurityReference
+import springfox.documentation.service.Tag
 import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spi.service.contexts.SecurityContext
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
-import springfox.documentation.service.BasicAuth
-import java.util.ArrayList
-import springfox.documentation.service.SecurityScheme
-import springfox.documentation.service.SecurityReference
-import springfox.documentation.spi.service.contexts.SecurityContext
-import springfox.documentation.spring.web.paths.AbstractPathProvider
-import springfox.documentation.spring.web.paths.RelativePathProvider
-import javax.servlet.ServletContext
 
 
 @Configuration
@@ -36,6 +32,36 @@ class SwaggerConfig {
                 .securityContexts(securityContexts)
                 .consumes(setOf("application/json"))
                 .produces(setOf("application/json"))
+                .tags(
+                        Tag("accesslog", "Access logs base API"),
+                        Tag("code", "Codes CRUD and advanced API"),
+                        Tag("contact", "Contacts CRUD and advanced API"),
+                        Tag("document", "Documents CRUD and advanced API"),
+                        Tag("entitytemplate", "Entity templates CRUD and advanced API"),
+                        Tag("doctemplate", "Entity templates CRUD and advanced API"),
+                        Tag("filter", "Entity templates CRUD and advanced API"),
+                        Tag("form", "Forms CRUD and advanced API"),
+                        Tag("generic", "iCure generic actions API"),
+                        Tag("group", "Practice groups API"),
+                        Tag("hcparty", "Healthcare parties CRUD and advanced API"),
+                        Tag("helement", "Health elements CRUD and advanced API"),
+                        Tag("icure", "iCure application basic API"),
+                        Tag("insurance", "Insurances CRUD and advanced API"),
+                        Tag("invoice", "Invoices CRUD and advanced API"),
+                        Tag("auth", "Authentification API"),
+                        Tag("message", "Messages CRUD and advanced API"),
+                        Tag("patient", "Patients CRUD and advanced API"),
+                        Tag("replication", "Replication API"),
+                        Tag("tarification", "Tarifications CRUD and advanced API"),
+                        Tag("technicaladmin", "Technical internal API"),
+                        Tag("user", "Users CRUD and advanced API"),
+                        Tag("be_drugs", "API for belgian Drugs service"),
+                        Tag("be_mikrono", "API for belgian Mikrono service"),
+                        Tag("be_progenda", "API for belgian Progenda service"),
+                        Tag("be_kmehr", "API for belgian Kmehr service"),
+                        Tag("be_result_import", "API for belgian Result_import service"),
+                        Tag("be_result_export", "API for belgian Result_export service")
+                )
                 .select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build()
     }
 }
