67644eeaf6d887b9719ef575185211980ed4aa71
Antoine Duchâteau <ad@taktik.com>
2019-11-04 15:30:33 +0100

    Fix merge on accesslogs

--- src/main/java/org/taktik/icure/dao/AccessLogDAO.java
+++ src/main/java/org/taktik/icure/dao/AccessLogDAO.java
@@ -33,5 +33,4 @@ public interface AccessLogDAO extends GenericDAO<AccessLog> {
 	PaginatedList<AccessLog> list(PaginationOffset paginationOffset, boolean descending);
 	PaginatedList<AccessLog> findByUserAfterDate(String userId, String accessType, Instant startDate, PaginationOffset<List<Object>> pagination, boolean descending);
 	List<AccessLog> findByHCPartySecretPatientKeys(String hcPartyId, List<String> secretPatientKeys);
-	List<AccessLog> findByHCPartySecretPatientKeys(String hcPartyId, ArrayList<String> secretForeignKeys);
 }
