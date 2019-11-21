906ea499ee662a3fe3aa5c0900cf021ae2596af7
Simon Hardy <sha@taktik.com>
2019-11-21 12:00:10 +0100

    add DAOs

---
+++ src/main/kotlin/org/taktik/icure/asyncdao/ReplicationDAO.kt
@@ -0,0 +1,14 @@
+package org.taktik.icure.asyncdao
+
+import org.ektorp.ReplicationTask
+import org.taktik.icure.entities.Replication
+import org.taktik.icure.entities.embed.DatabaseSynchronization
+import java.net.URI
+
+interface ReplicationDAO {
+    suspend fun getByName(dbInstanceUrl: URI, groupId: String, name: String): Replication?
+
+    fun getActiveReplications(): List<ReplicationTask>
+    fun startReplication(databaseSynchronization: DatabaseSynchronization, continuous: Boolean)
+    fun cancelReplication(databaseSynchronization: DatabaseSynchronization)
+}
---
+++ src/main/kotlin/org/taktik/icure/asyncdao/RoleDAO.kt
@@ -0,0 +1,8 @@
+package org.taktik.icure.asyncdao
+
+import org.taktik.icure.entities.Role
+import java.net.URI
+
+interface RoleDAO {
+    suspend fun getByName(dbInstanceUrl: URI, groupId: String, name: String): Role?
+}
