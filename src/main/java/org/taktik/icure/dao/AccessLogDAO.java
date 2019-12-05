d73b7c75fd5a59df0dc69b9617b147b61985f7d8
Antoine Duchâteau <ad@taktik.com>
2019-12-05 21:15:57 +0100

    Fix for accessLogs endpoints

--- src/main/java/org/taktik/icure/dao/AccessLogDAO.java
+++ src/main/java/org/taktik/icure/dao/AccessLogDAO.java
@@ -30,7 +30,7 @@ import java.util.List;
 
 public interface AccessLogDAO extends GenericDAO<AccessLog> {
 
-	PaginatedList<AccessLog> list(PaginationOffset paginationOffset, boolean descending);
+	PaginatedList<AccessLog> list(Long fromEpoch, Long toEpoch, PaginationOffset paginationOffset, boolean descending);
 	PaginatedList<AccessLog> findByUserAfterDate(String userId, String accessType, Instant startDate, PaginationOffset<List<Object>> pagination, boolean descending);
 	List<AccessLog> findByHCPartySecretPatientKeys(String hcPartyId, List<String> secretPatientKeys);
 }
