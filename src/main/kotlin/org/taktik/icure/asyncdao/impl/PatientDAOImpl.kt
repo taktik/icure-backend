fb00a7467f1cd2f7568ec081eb1549341cf491e9
Simon Hardy <sha@taktik.com>
2019-12-11 15:06:51 +0100

    Fix reifined methods incorrectly used (2)

--- src/main/kotlin/org/taktik/icure/asyncdao/impl/PatientDAOImpl.kt
+++ src/main/kotlin/org/taktik/icure/asyncdao/impl/PatientDAOImpl.kt
@@ -227,7 +227,7 @@ class PatientDAOImpl(@Qualifier("patientCouchDbDispatcher") couchDbDispatcher: C
     override fun findIdsByHcParty(dbInstanceUrl: URI, groupId: String, healthcarePartyId: String, pagination: PaginationOffset<ComplexKey>): Flow<ViewQueryResultEvent> {
         val client = couchDbDispatcher.getClient(dbInstanceUrl, groupId)
         val viewQuery = pagedViewQueryOfIds("by_hcparty_name", ComplexKey.of(healthcarePartyId, null), ComplexKey.of(healthcarePartyId, ComplexKey.emptyObject()), pagination)
-        return client.queryView(viewQuery, ComplexKey::class.java, String::class.java)
+        return client.queryView(viewQuery, ComplexKey::class.java, String::class.java, Any::class.java)
     }
 
     override fun findPatientsByHcPartyAndName(dbInstanceUrl: URI, groupId: String, name: String, healthcarePartyId: String, pagination: PaginationOffset<ComplexKey>, descending: Boolean): Flow<ViewQueryResultEvent> {
