f49c65dc448fa6c22022fa1c4e7315ebcddbe655
Simon Hardy <sha@taktik.com>
2020-01-03 14:17:07 +0100

    scanning

--- src/main/kotlin/org/taktik/icure/ICureBackendApplication.kt
+++ src/main/kotlin/org/taktik/icure/ICureBackendApplication.kt
@@ -19,7 +19,6 @@
 
 package org.taktik.icure
 
-import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.ApplicationRunner
@@ -31,28 +30,24 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.autoconfigure.jdbc.JndiDataSourceAutoConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.PropertySource
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver
 import org.springframework.core.task.TaskExecutor
 import org.springframework.scheduling.TaskScheduler
 import org.taktik.icure.asyncdao.GenericDAO
 import org.taktik.icure.asynclogic.CodeLogic
 import org.taktik.icure.asynclogic.ICureLogic
 import org.taktik.icure.asynclogic.PropertyLogic
-import org.taktik.icure.entities.embed.*
 
 @SpringBootApplication(scanBasePackages = [
     "org.springframework.boot.autoconfigure.aop",
     "org.springframework.boot.autoconfigure.context",
     "org.springframework.boot.autoconfigure.dao",
-    //"org.springframework.boot.autoconfigure.jackson", TODO SH later: can remove this?
-    //"org.springframework.boot.autoconfigure.jersey",
     "org.springframework.boot.autoconfigure.validation",
     "org.springframework.boot.autoconfigure.websocket",
     "org.taktik.icure.config",
     "org.taktik.icure.asyncdao",
     "org.taktik.icure.asynclogic",
     "org.taktik.icure.be.ehealth.logic",
-    "org.taktik.icure.be.drugs.logic",
+//    "org.taktik.icure.be.drugs.logic",
     "org.taktik.icure.be.format.logic",
 //    "org.taktik.icure.be.samv2.logic",
     "org.taktik.icure.properties",
