86da743d68aa44ca0f71ca1b4ae6caf6fd400a07
Antoine Duchâteau <ad@taktik.com>
2020-01-09 19:12:33 +0100

    Make retry tail recursive

--- src/main/kotlin/org/taktik/icure/utils/LanguageUtils.kt
+++ src/main/kotlin/org/taktik/icure/utils/LanguageUtils.kt
@@ -1,25 +1,23 @@
 package org.taktik.icure.utils
 
-fun <K>retry(trials: Int, closure: () -> K): K {
-    return try {
-        closure()
+tailrec fun <K>retry(trials: Int, closure: () -> K): K {
+    try {
+        return closure()
     } catch(e: Exception) {
-        if (trials>1) {
-            retry(trials - 1, closure)
-        } else {
+        if (trials < 1) {
             throw e
         }
     }
+    return retry(trials - 1, closure)
 }
 
-suspend fun <K>suspendRetry(trials: Int, closure: suspend () -> K): K {
-    return try {
-        closure()
+tailrec suspend fun <K>suspendRetry(trials: Int, closure: suspend () -> K): K {
+    try {
+        return closure()
     } catch(e: Exception) {
-        if (trials>1) {
-            suspendRetry(trials - 1, closure)
-        } else {
+        if (trials < 1) {
             throw e
         }
     }
+    return suspendRetry(trials - 1, closure)
 }
