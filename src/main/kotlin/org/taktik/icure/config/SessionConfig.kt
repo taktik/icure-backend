a2b5e6396898c491f79202128efad908ac50c5b3
Antoine Duchâteau <ad@taktik.com>
2020-02-07 14:45:33 +0100

    Move java files to java sources

--- src/main/kotlin/org/taktik/icure/config/SessionConfig.kt
+++ src/main/kotlin/org/taktik/icure/config/SessionConfig.kt
@@ -7,9 +7,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.Ordered
 import org.springframework.session.MapSession
-import org.springframework.session.ReactiveMapSessionRepository
 import org.springframework.session.ReactiveSessionRepository
-import org.springframework.session.Session
 import org.springframework.session.config.annotation.web.server.EnableSpringWebSession
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping
 import org.springframework.web.reactive.socket.server.WebSocketService
