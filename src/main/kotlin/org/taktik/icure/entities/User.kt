52755c58d2b0a9b25e8a895cf4b831118ffd6e59
Antoine Duchâteau <ad@taktik.com>
2020-06-26 14:56:23 +0200

    Fixed user update

--- src/main/kotlin/org/taktik/icure/entities/User.kt
+++ src/main/kotlin/org/taktik/icure/entities/User.kt
@@ -17,6 +17,7 @@
  */
 package org.taktik.icure.entities
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
@@ -135,5 +136,6 @@ data class User(
     override fun withIdRev(id: String?, rev: String) = if (id != null) this.copy(id = id, rev = rev) else this.copy(rev = rev)
     override fun withDeletionDate(deletionDate: Long?) = this.copy(deletionDate = deletionDate)
 
+    @JsonIgnore
     override fun getParents(): Set<String> = this.roles
 }
