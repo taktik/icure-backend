133919b9b947cf9742ae4668cf179760fa02a3ef
Adrian <adrian.meyvn@gmail.com>
2020-11-26 16:52:07 +0200

    couchdb view fix

--- src/main/resources/org/taktik/icure/dao/samv2/impl/js/amp/By_dmppcode.js
+++ src/main/resources/org/taktik/icure/dao/samv2/impl/js/amp/By_dmppcode.js
@@ -2,6 +2,6 @@ map = function(doc) {
     if (doc.java_type === 'org.taktik.icure.entities.samv2.Amp' && !doc.deleted && doc.ampps && doc.ampps.length) {
         var dmpps = {}
         doc.ampps.forEach(function(ampp) { ampp && ampp.dmpps && ampp.dmpps.forEach(function(dmpp) { dmpp && dmpp.code && (dmpps[dmpp.code] = 1)})})
-        Object.keys(dmpps).forEach(k => emit(k,1))
+        Object.keys(dmpps).forEach(function(k) {emit(k,1)})
     }
 };
