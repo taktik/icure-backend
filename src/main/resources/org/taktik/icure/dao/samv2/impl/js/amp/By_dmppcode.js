151655fc66a459180b1ca0e0c7f5fbf4ded3eca4
Antoine Duchâteau <ad@taktik.com>
2020-11-26 11:27:30 +0100

    Fix VMPGroup/bygroupcode
    (cherry picked from commit 9ce2eb1514aea3d3b5e07299e65571188efb52f4)

--- src/main/resources/org/taktik/icure/dao/samv2/impl/js/amp/By_dmppcode.js
+++ src/main/resources/org/taktik/icure/dao/samv2/impl/js/amp/By_dmppcode.js
@@ -1,5 +1,7 @@
 map = function(doc) {
     if (doc.java_type === 'org.taktik.icure.entities.samv2.Amp' && !doc.deleted && doc.ampps && doc.ampps.length) {
-        doc.ampps.forEach(function(ampp) { ampp && ampp.dmpps && ampp.dmpps.forEach(function(dmpp) { dmpp && dmpp.code && emit(dmpp.code, 1)})})
+        var dmpps = {}
+        doc.ampps.forEach(function(ampp) { ampp && ampp.dmpps && ampp.dmpps.forEach(function(dmpp) { dmpp && dmpp.code && (dmpps[dmpp.code] = 1)})})
+        Object.keys(dmpps).forEach(k => emit(k,1))
     }
 };
