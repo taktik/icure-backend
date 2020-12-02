82c451352842c47435543888bc539cbc77098329
philippe_mertens <mertensph@gmail.com>
2020-12-02 21:26:24 +0100

    Redirect Exceptions import

--- src/main/kotlin/org/taktik/couchdb/support/StdDesignDocumentFactory.kt
+++ src/main/kotlin/org/taktik/couchdb/support/StdDesignDocumentFactory.kt
@@ -19,7 +19,6 @@ package org.taktik.couchdb.support
 
 import org.apache.commons.io.IOUtils
 import org.ektorp.util.Assert
-import org.ektorp.util.Exceptions
 import org.taktik.couchdb.annotation.Filter
 import org.taktik.couchdb.annotation.Filters
 import org.taktik.couchdb.annotation.ListFunction
@@ -29,6 +28,7 @@ import org.taktik.couchdb.annotation.Shows
 import org.taktik.couchdb.annotation.UpdateHandler
 import org.taktik.couchdb.annotation.UpdateHandlers
 import org.taktik.couchdb.entity.DesignDocument
+import org.taktik.couchdb.util.Exceptions
 import org.taktik.couchdb.util.ReflectionUtils
 import java.io.FileNotFoundException
 import java.util.*
