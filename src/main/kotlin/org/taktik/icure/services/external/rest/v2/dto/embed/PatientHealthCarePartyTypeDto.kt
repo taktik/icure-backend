2ada13e98a2b8812c1052bd370d2e67be29533cc
Clément Vandendaelen <cv@taktik.com>
2021-11-03 17:23:58 +0100

    Changed referralHospital to managingOrganization
    Signed-off-by: Clément Vandendaelen <cv@taktik.com>

--- src/main/kotlin/org/taktik/icure/services/external/rest/v2/dto/embed/PatientHealthCarePartyTypeDto.kt
+++ src/main/kotlin/org/taktik/icure/services/external/rest/v2/dto/embed/PatientHealthCarePartyTypeDto.kt
@@ -21,5 +21,5 @@ import org.taktik.icure.services.external.rest.v2.dto.base.EnumVersionDto
 
 @EnumVersionDto(1L)
 enum class PatientHealthCarePartyTypeDto {
-    doctor, referral, medicalhouse, retirementhome, hospital, other, referringphysician, referralHospital
+    doctor, referral, medicalhouse, retirementhome, hospital, other, referringphysician, managingOrganization
 }
