dd7276d290385daa838197e793841a317f873799
Clément Vandendaelen <cv@taktik.com>
2021-11-29 12:50:32 +0100

    Added missing CouchDB Views
    Signed-off-by: Clément Vandendaelen <cv@taktik.com>

---
+++ src/main/resources/org/taktik/icure/asyncdao/impl/js/contact/By_hcparty_serviceid_map.js
@@ -0,0 +1,27 @@
+function(doc) {
+  var emit_contacts = function (k, doc) {
+    var idsMap = {};
+    doc.services.forEach(function (s) {
+      if (s._id) {
+        if (!idsMap[s._id]) {
+          idsMap[s._id] = 1;
+          emit([k, s._id], doc._id);
+        }
+      }
+    });
+    doc.services.forEach(function (sc) {
+      if (sc._id) {
+        if (!idsMap[sc._id]) {
+          idsMap[sc._id] = 1;
+          emit([k, sc._id], doc._id);
+        }
+      }
+    });
+  };
+
+  if (doc.java_type === 'org.taktik.icure.entities.Contact' && !doc.deleted && doc.secretForeignKeys && doc.secretForeignKeys.length && doc.delegations && Object.keys(doc.delegations).length) {
+    Object.keys(doc.delegations).forEach(function (k) {
+     emit_contacts(k, doc);
+    });
+  }
+}
