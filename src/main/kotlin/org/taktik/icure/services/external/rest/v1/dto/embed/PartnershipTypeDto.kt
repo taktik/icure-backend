237e3ee7ac8aa81fc9f16f4d26d4d91bc2832853
Clément Vandendaelen <cv@taktik.com>
2021-12-01 16:01:56 +0100

    ICURE-355 Added seealso and refer
    Signed-off-by: Clément Vandendaelen <cv@taktik.com>

--- src/main/kotlin/org/taktik/icure/services/external/rest/v1/dto/embed/PartnershipTypeDto.kt
+++ src/main/kotlin/org/taktik/icure/services/external/rest/v1/dto/embed/PartnershipTypeDto.kt
@@ -23,5 +23,6 @@ import org.taktik.icure.services.external.rest.v1.dto.base.EnumVersionDto
 enum class PartnershipTypeDto {
     primary_contact, primary_contact_for, family, friend, counselor, contact,  //From Kmehr
     brother, brotherinlaw, child, daughter, employer, father, grandchild, grandparent, husband, lawyer, mother, neighbour, notary, partner, sister, sisterinlaw, son, spouse, stepdaughter, stepfather, stepmother, stepson, tutor,
-    next_of_kin, federal_agency, insurance_company, state_agency, unknown //from FHIR : http://terminology.hl7.org/CodeSystem/v2-0131
+    next_of_kin, federal_agency, insurance_company, state_agency, unknown, //from FHIR : http://terminology.hl7.org/CodeSystem/v2-0131
+    seealso, refer //from FHIR : https://www.hl7.org/fhir/codesystem-link-type.html
 }
