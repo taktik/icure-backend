284b17531ba76a1c3e8f96a9cc8b60da0f9f5351
Clément Vandendaelen <cv@taktik.com>
2021-12-01 14:16:58 +0100

    Removed FHIR code
    Signed-off-by: Clément Vandendaelen <cv@taktik.com>

--- src/main/kotlin/org/taktik/icure/services/external/rest/v1/dto/embed/PartnershipTypeDto.kt
+++ src/main/kotlin/org/taktik/icure/services/external/rest/v1/dto/embed/PartnershipTypeDto.kt
@@ -20,8 +20,8 @@ package org.taktik.icure.services.external.rest.v1.dto.embed
 import org.taktik.icure.services.external.rest.v1.dto.base.EnumVersionDto
 
 @EnumVersionDto(1L)
-enum class PartnershipTypeDto(val fhirCode: String? = null) {
-    primary_contact("C"), primary_contact_for, family, friend, counselor, contact,  //From Kmehr
-    brother, brotherinlaw, child, daughter, employer("E"), father, grandchild, grandparent, husband, lawyer, mother, neighbour, notary, partner, sister, sisterinlaw, son, spouse, stepdaughter, stepfather, stepmother, stepson, tutor,
-    next_of_kin("N"), federal_agency("F"), insurance_company("I"), state_agency("S"), unknown("U") //from FHIR : http://terminology.hl7.org/CodeSystem/v2-0131
+enum class PartnershipTypeDto {
+    primary_contact, primary_contact_for, family, friend, counselor, contact,  //From Kmehr
+    brother, brotherinlaw, child, daughter, employer, father, grandchild, grandparent, husband, lawyer, mother, neighbour, notary, partner, sister, sisterinlaw, son, spouse, stepdaughter, stepfather, stepmother, stepson, tutor,
+    next_of_kin, federal_agency, insurance_company, state_agency, unknown //from FHIR : http://terminology.hl7.org/CodeSystem/v2-0131
 }
