46f8bd2dbabd582a413ecae7e67424b0ae43b77b
Clément Vandendaelen <cv@taktik.com>
2021-12-01 11:24:50 +0100

    Fixed typos
    Signed-off-by: Clément Vandendaelen <cv@taktik.com>

--- src/main/kotlin/org/taktik/icure/entities/embed/PartnershipType.kt
+++ src/main/kotlin/org/taktik/icure/entities/embed/PartnershipType.kt
@@ -23,5 +23,5 @@ import org.taktik.icure.entities.base.EnumVersion
 enum class PartnershipType {
     primary_contact, primary_contact_for, family, friend, counselor, contact,  //From Kmehr
     brother, brotherinlaw, child, daughter, employer, father, grandchild, grandparent, husband, lawyer, mother, neighbour, notary, partner, sister, sisterinlaw, son, spouse, stepdaughter, stepfather, stepmother, stepson, tutor,
-    nextofkin, federalagency, insurancecompany, stateagency, other, unknown //from FHIR : http://terminology.hl7.org/CodeSystem/v2-0131
+    next_of_kin, federal_agency, insurance_company, state_agency, other, unknown //from FHIR : http://terminology.hl7.org/CodeSystem/v2-0131
 }
