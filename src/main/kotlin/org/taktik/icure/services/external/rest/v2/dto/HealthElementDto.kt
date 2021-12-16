ea372773b0e879082bae4dcf6d1279d542e26ccd
Antoine Duchâteau <ad@taktik.com>
2021-12-16 12:50:30 +0100

    Final Alignment

--- src/main/kotlin/org/taktik/icure/services/external/rest/v2/dto/HealthElementDto.kt
+++ src/main/kotlin/org/taktik/icure/services/external/rest/v2/dto/HealthElementDto.kt
@@ -22,10 +22,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.github.pozo.KotlinBuilder
 import io.swagger.v3.oas.annotations.media.Schema
-import org.taktik.icure.services.external.rest.v2.dto.base.CodeStubDto
-import org.taktik.icure.services.external.rest.v2.dto.base.EncryptableDto
-import org.taktik.icure.services.external.rest.v2.dto.base.ICureDocumentDto
-import org.taktik.icure.services.external.rest.v2.dto.base.StoredDocumentDto
+import org.taktik.icure.services.external.rest.v2.dto.base.*
 import org.taktik.icure.services.external.rest.v2.dto.embed.*
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -34,6 +31,7 @@ import org.taktik.icure.services.external.rest.v2.dto.embed.*
 @Schema(description = """This entity is a root level object. It represents a healthcare element. It is serialized in JSON and saved in the underlying CouchDB database.""")
 data class HealthElementDto(
         @Schema(description = "The Id of the healthcare element. We encourage using either a v4 UUID or a HL7 Id.") override val id: String,
+        val identifier: List<IdentifierDto> = listOf(),
         @Schema(description = "The revision of the patient in the database, used for conflict management / optimistic locking.") override val rev: String? = null,
         override val created: Long? = null,
         override val modified: Long? = null,
