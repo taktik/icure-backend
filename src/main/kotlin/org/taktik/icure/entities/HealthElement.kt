118effd27370e54d116f216f7270d9a95afd9630
Antoine Duchâteau <ad@taktik.com>
2021-12-21 12:11:02 +0100

    Add missing identifier

--- src/main/kotlin/org/taktik/icure/entities/HealthElement.kt
+++ src/main/kotlin/org/taktik/icure/entities/HealthElement.kt
@@ -25,12 +25,7 @@ import org.taktik.couchdb.entity.Attachment
 import org.taktik.icure.entities.base.CodeStub
 import org.taktik.icure.entities.base.Encryptable
 import org.taktik.icure.entities.base.StoredICureDocument
-import org.taktik.icure.entities.embed.CareTeamMember
-import org.taktik.icure.entities.embed.Delegation
-import org.taktik.icure.entities.embed.Episode
-import org.taktik.icure.entities.embed.Laterality
-import org.taktik.icure.entities.embed.PlanOfAction
-import org.taktik.icure.entities.embed.RevisionInfo
+import org.taktik.icure.entities.embed.*
 import org.taktik.icure.entities.utils.MergeUtil.mergeListsDistinct
 import org.taktik.icure.utils.DynamicInitializer
 import org.taktik.icure.utils.invoke
@@ -96,6 +91,7 @@ import javax.validation.Valid
 data class HealthElement(
         @JsonProperty("_id") override val id: String,
         @JsonProperty("_rev") override val rev: String? = null,
+        val identifier: List<Identifier> = listOf(),
         @field:NotNull(autoFix = AutoFix.NOW) override val created: Long? = null,
         @field:NotNull(autoFix = AutoFix.NOW) override val modified: Long? = null,
         @field:NotNull(autoFix = AutoFix.CURRENTUSERID) override val author: String? = null,
@@ -138,6 +134,9 @@ data class HealthElement(
 
     fun merge(other: HealthElement) = HealthElement(args = this.solveConflictsWith(other))
     fun solveConflictsWith(other: HealthElement) = super<StoredICureDocument>.solveConflictsWith(other) + super<Encryptable>.solveConflictsWith(other) + mapOf(
+            "identifier" to mergeListsDistinct(this.identifier, other.identifier,
+                    { a, b -> a.system == b.system && a.value == b.value },
+            ),
             "healthElementId" to (this.healthElementId ?: other.healthElementId),
             "valueDate" to (valueDate?.coerceAtMost(other.valueDate ?: Long.MAX_VALUE) ?: other.valueDate),
             "openingDate" to (openingDate?.coerceAtMost(other.openingDate ?: Long.MAX_VALUE) ?: other.openingDate),
