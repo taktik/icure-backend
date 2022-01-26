9dd066d62a8279f9a1efbb93f675426287ad84c0
Antoine Duchâteau <ad@taktik.com>
2022-01-26 10:50:13 +0100

    Fix Reimbursement in SAM

--- src/main/kotlin/org/taktik/icure/services/external/rest/v1/dto/samv2/embed/ReimbursementDto.kt
+++ src/main/kotlin/org/taktik/icure/services/external/rest/v1/dto/samv2/embed/ReimbursementDto.kt
@@ -18,8 +18,14 @@
 
 package org.taktik.icure.services.external.rest.v1.dto.samv2.embed
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.github.pozo.KotlinBuilder
 import java.math.BigDecimal
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@KotlinBuilder
 data class ReimbursementDto(
         override val from: Long? = null,
         override val to: Long? = null,
