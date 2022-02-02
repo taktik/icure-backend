f76ed5fd2cdeb2907b74d69b26535ecd78650b43
Clément Vandendaelen <cv@taktik.com>
2022-02-02 13:34:02 +0100

    Added createDevices logic
    Signed-off-by: Clément Vandendaelen <cv@taktik.com>

--- src/main/kotlin/org/taktik/icure/asynclogic/impl/DeviceLogicImpl.kt
+++ src/main/kotlin/org/taktik/icure/asynclogic/impl/DeviceLogicImpl.kt
@@ -3,9 +3,10 @@ package org.taktik.icure.asynclogic.impl
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
 import org.springframework.stereotype.Service
 import org.taktik.couchdb.DocIdentifier
-import org.taktik.couchdb.ViewQueryResultEvent
 import org.taktik.icure.asyncdao.DeviceDAO
 import org.taktik.icure.asynclogic.AsyncSessionLogic
 import org.taktik.icure.asynclogic.DeviceLogic
@@ -19,14 +20,14 @@ import org.taktik.icure.entities.Device
 class DeviceLogicImpl(private val sessionLogic: AsyncSessionLogic,
                       private val deviceDAO: DeviceDAO,
                       private val userLogic: UserLogic,
-                      private val filters: Filters): DeviceLogic {
+                      private val filters: Filters): DeviceLogic, GenericLogicImpl<Device, DeviceDAO>(sessionLogic) {
 
     override suspend fun createDevice(device: Device): Device? {
         TODO("Not yet implemented")
     }
 
-    override fun createDevices(devices: List<Device>): Flow<Device> {
-        TODO("Not yet implemented")
+    override fun createDevices(devices: List<Device>): Flow<Device> = flow {
+        emitAll(createEntities(devices.map { fix(it) }))
     }
 
     override suspend fun modifyDevice(device: Device): Device? {
@@ -47,5 +48,9 @@ class DeviceLogicImpl(private val sessionLogic: AsyncSessionLogic,
 
     override fun deleteDevices(ids: Collection<String>): Flow<DocIdentifier> {
         TODO("Not yet implemented")
+    }
+
+    override fun getGenericDAO(): DeviceDAO {
+        return deviceDAO
     }
 }
