9c456d0d02a10bf671384284801da8fb23aff2dd
Damien Libouton <damien.libouton@medispring.be>
2022-03-22 17:03:18 +0100

    MS-7384: update mapping of ReplicateCommandDto

--- src/main/kotlin/org/taktik/icure/services/external/rest/v1/dto/ReplicateCommandDto.kt
+++ src/main/kotlin/org/taktik/icure/services/external/rest/v1/dto/ReplicateCommandDto.kt
@@ -9,11 +9,11 @@ import com.github.pozo.KotlinBuilder
 @JsonIgnoreProperties(ignoreUnknown = true)
 @KotlinBuilder
 data class ReplicateCommandDto(
-        @JsonProperty("source_url") val sourceUrl: String,
-        @JsonProperty("source_username") val sourceUsername: String,
-        @JsonProperty("source_password") val sourcePassword: String,
-        @JsonProperty("target_url") val targetUrl: String,
-        @JsonProperty("target_username") val targetUsername: String,
-        @JsonProperty("target_password") val targetPassword: String,
+        val sourceUrl: String,
+        val sourceUsername: String,
+        val sourcePassword: String,
+        val targetUrl: String,
+        val targetUsername: String,
+        val targetPassword: String,
         val id: String? = null
 )
