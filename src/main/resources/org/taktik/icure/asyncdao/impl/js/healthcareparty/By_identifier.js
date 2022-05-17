35c28bb6a8c58072724ddf1d33b5b69fd336ff41
Clément Vandendaelen <cv@taktik.com>
2022-05-17 17:18:26 +0200

    Fixed view
    Signed-off-by: Clément Vandendaelen <cv@taktik.com>

--- src/main/resources/org/taktik/icure/asyncdao/impl/js/healthcareparty/By_identifier.js
+++ src/main/resources/org/taktik/icure/asyncdao/impl/js/healthcareparty/By_identifier.js
@@ -1,12 +1,10 @@
-map = function(doc) {
+map = function (doc) {
     var emit_hcp = function (doc) {
-        doc.identifier.forEach(function (identifier) {
-            if (identifier.identifier && identifier.identifier.length) {
-                identifier.identifier.forEach(function (identifier) {
-                    emit([identifier.system, identifier.value], doc._id);
-                });
-            }
-        });
+        if (doc.identifier && doc.identifier.length) {
+            doc.identifier.forEach(function (identifier) {
+                emit([identifier.system, identifier.value], doc._id);
+            });
+        };
     };
 
     if (doc.java_type === 'org.taktik.icure.entities.HealthcareParty' && !doc.deleted) {
