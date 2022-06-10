0a28c4911c59c21c3f6f72e6519cc092db954233
Clément Vandendaelen <cv@taktik.com>
2022-06-10 10:47:46 +0200

    Fixed compilation error
    Signed-off-by: Clément Vandendaelen <cv@taktik.com>

--- src/test/kotlin/org/taktik/icure/entities/PropertyTest.kt
+++ src/test/kotlin/org/taktik/icure/entities/PropertyTest.kt
@@ -3,6 +3,7 @@ package org.taktik.icure.entities
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import org.mapstruct.factory.Mappers
+import org.taktik.icure.entities.embed.Identifier
 import org.taktik.icure.entities.embed.TypedValue
 import org.taktik.icure.services.external.rest.v1.dto.PropertyDto
 import org.taktik.icure.services.external.rest.v1.mapper.PropertyMapper
@@ -10,7 +11,7 @@ import org.taktik.icure.services.external.rest.v1.mapper.PropertyMapper
 class PropertyTest {
 	@Test
 	fun map() {
-		val result: PropertyDto = Mappers.getMapper(PropertyMapper::class.java).map(Property(id = "123", type = PropertyType(id = "123"), typedValue = TypedValue.withValue(0)))
+		val result: PropertyDto = Mappers.getMapper(PropertyMapper::class.java).map(Property(id = "123", type = PropertyType(id = "123", identifier = "456"), typedValue = TypedValue.withValue(0)))
 		assertNotNull(result)
 	}
 }
