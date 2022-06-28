42c8b142258a41cabe19ddf678c53bb1e62da205
Luca Tremamunno <luca@icure.com>
2022-06-28 17:23:10 +0200

    Add object storage client implementation

--- src/main/kotlin/org/taktik/icure/asynclogic/objectstorage/impl/ObjectStorageClientImpl.kt
+++ src/main/kotlin/org/taktik/icure/asynclogic/objectstorage/impl/ObjectStorageClientImpl.kt
@@ -4,15 +4,18 @@ import java.util.Base64
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.reactive.asPublisher
+import kotlinx.coroutines.reactive.awaitFirst
 import org.slf4j.LoggerFactory
 import org.springframework.core.io.buffer.DataBuffer
 import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.client.reactive.ClientHttpRequest
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.BodyInserter
 import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitBodilessEntity
 import org.springframework.web.reactive.function.client.awaitBody
 import org.springframework.web.reactive.function.client.bodyToFlow
 import org.springframework.web.util.DefaultUriBuilderFactory
@@ -20,6 +23,7 @@ import org.taktik.icure.asynclogic.AsyncSessionLogic
 import org.taktik.icure.asynclogic.objectstorage.ObjectStorageClient
 import org.taktik.icure.properties.ObjectStorageProperties
 import org.taktik.icure.security.database.DatabaseUserDetails
+import reactor.core.publisher.Mono
 
 @Service
 @ExperimentalCoroutinesApi
@@ -50,9 +54,9 @@ class ObjectStorageClientImpl(
 				.setUriAndAuthorization(documentId, attachmentId)
 				.body(bodyInserter)
 				.retrieve()
-				.awaitBody<Any>()
-		}.also { r ->
-			r.exceptionOrNull()?.let { log.warn("Failed to upload attachment $attachmentId@$documentId", it) }
+				.awaitBodilessEntity()
+		}.onFailure {
+			log.warn("Failed to upload attachment $attachmentId@$documentId", it)
 		}.isSuccess
 
 	override suspend fun get(documentId: String, attachmentId: String): Flow<DataBuffer> =
@@ -62,17 +66,24 @@ class ObjectStorageClientImpl(
 			.bodyToFlow()
 
 	override suspend fun checkAvailable(documentId: String, attachmentId: String): Boolean =
-		TODO("Head request or similar")
-
-	override suspend fun delete(documentId: String, attachmentId: String): Boolean =
 		runCatching {
-			icureCloudClient.delete()
+			icureCloudClient.head()
 				.setUriAndAuthorization(documentId, attachmentId)
-				.retrieve()
-				.awaitBody<Any>()
-		}.also { r ->
-			r.exceptionOrNull()?.let { log.warn("Failed to delete attachment $attachmentId@$documentId", it) }
-		}.isSuccess
+				.exchangeToMono {
+					when (it.statusCode()) {
+						HttpStatus.OK, HttpStatus.NO_CONTENT -> Mono.just(true)
+						HttpStatus.NOT_FOUND -> Mono.just(false)
+						else -> it.createException().flatMap { e -> Mono.error(e) }
+					}
+				}
+				.awaitFirst()
+		}.onFailure {
+			log.warn("Error while checking availability of attachment $attachmentId@$documentId", it)
+		}.getOrNull() == true
+
+	// Deletion is actually handled by maintenance tasks
+	override suspend fun delete(documentId: String, attachmentId: String): Boolean =
+		true
 
 	private suspend fun <T : WebClient.RequestHeadersSpec<T>> WebClient.RequestHeadersUriSpec<T>.setUriAndAuthorization(
 		documentId: String,
@@ -86,7 +97,8 @@ class ObjectStorageClientImpl(
 			authHeader()?.let { queryBuilder.header("Authorization", it) } ?: queryBuilder
 		}
 
-	private fun attachmentRoute(documentId: String, attachmentId: String): Array<String> = TODO()
+	private fun attachmentRoute(documentId: String, attachmentId: String): Array<String> =
+		arrayOf("rest", "v2", "objectstorage", "documents", documentId, attachmentId)
 
 	private suspend fun authHeader() =
 		(sessionLogic.getCurrentSessionContext().getUserDetails() as? DatabaseUserDetails)
