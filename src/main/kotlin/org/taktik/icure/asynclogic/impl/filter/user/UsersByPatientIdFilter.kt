7e64826c14317fb8d61044aa11d936976ca7bc92
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2022-07-11 10:03:15 +0200

    Changed the class to a more specific interface

--- src/main/kotlin/org/taktik/icure/asynclogic/impl/filter/user/UsersByPatientIdFilter.kt
+++ src/main/kotlin/org/taktik/icure/asynclogic/impl/filter/user/UsersByPatientIdFilter.kt
@@ -4,13 +4,14 @@ import kotlinx.coroutines.flow.Flow
 import org.springframework.stereotype.Service
 import org.taktik.icure.asynclogic.UserLogic
 import org.taktik.icure.asynclogic.impl.filter.Filter
-import org.taktik.icure.domain.filter.Filters
+import org.taktik.icure.asynclogic.impl.filter.Filters
 import org.taktik.icure.entities.User
+import org.taktik.icure.domain.filter.user.UsersByPatientIdFilter
 
 @Service
-class UsersByPatientIdFilter(private val userLogic: UserLogic) : Filter<String, User, Filters.ByPatientIdFilter<String, User>> {
+class UsersByPatientIdFilter(private val userLogic: UserLogic) : Filter<String, User, UsersByPatientIdFilter> {
 
-	override fun resolve(filter: Filters.ByPatientIdFilter<String, User>, context: org.taktik.icure.asynclogic.impl.filter.Filters): Flow<String> {
+	override fun resolve(filter: UsersByPatientIdFilter, context: Filters): Flow<String> {
 		return userLogic.findByPatientId(filter.patientId)
 	}
 }
